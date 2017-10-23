07eb25d2342e3b8b51e574d18c9072f53fff9497
Pranav Tharoor <pranav.tharoor@gmail.com>
2017-10-23 20:44:49 +0000

    Fixed bug

--- config/auth.js
+++ config/auth.js
@@ -2,12 +2,12 @@ module.exports = {
 	'fbAuth': {
 		'clientID': '1439400149442376',
 		'clientSecret': 'ace695e9ae5aab460953236f25773138',
-		'callbackURL': 'http://localhost:3000/auth/facebook/callback'
+		'callbackURL': 'https://test.pranavtharoor.me/auth/facebook/callback/'
 	},
 	'googleAuth': {
 		'clientID': '1070235211205-h7jn8ut6o0rbngkvtclbomdo1ifr0utg.apps.googleusercontent.com',
 		'clientSecret': '0NHYwUrOhWpS8CEnNsMC1ejh',
-		'callbackURL': 'http://localhost:3000/auth/google/callback'
+		'callbackURL': 'https://test.pranavtharoor.me/auth/google/callback/'
 	}
 }
  
