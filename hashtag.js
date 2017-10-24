d11508a2fbc82974acbdd422939b15bab75078ed
Pranav Tharoor <pranav.tharoor@gmail.com>
2017-10-24 05:57:10 +0000

    Error handling

--- hashtag.js
+++ hashtag.js
@@ -35,6 +35,32 @@ function redirectUnmatched(req, res) {
   res.redirect('/');
 }
 
+/// catch 404 and forwarding to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+/// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+	console.log(err);
+        res.send('error');
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.send('Error occured');
+});
+
 const port = 3000;
 app.listen(port, () => {
 	console.log('Listening on port ' + port);
